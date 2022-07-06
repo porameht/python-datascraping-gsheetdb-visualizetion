6b4e3b29c5249eb6940140831ec12c135a1c555b
fr4nk <89307294+porameht@users.noreply.github.com>
2022-07-06 22:48:48 +0700

    extract data table from page 3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from anyio import TypedAttributeLookupError
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
+import pandas as pd
 import gspread
 import datetime
 
@@ -9,6 +10,7 @@ import datetime
 async def gethtml():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False, slow_mo=50)
+        # 
         page = await browser.new_page()
         # Login
         await page.goto("https://backoffice.nigoal939.com/login")
@@ -16,67 +18,79 @@ async def gethtml():
         await page.fill('input#password','aun331930')
         await page.click('a#login')
         await page.wait_for_url("https://backoffice.nigoal939.com/Dashboard")
+        print("Login successfully")
+
         # Logautobank
         await page.goto("https://backoffice.nigoal939.com/Logautobank")
         await page.wait_for_url("https://backoffice.nigoal939.com/Logautobank")
-        await page.wait_for_timeout(1000)
-        await page.click('a#deposit_manual-tab')
         await page.wait_for_timeout(3000)
-        # text = await page.inner_html('div.tab-pane fade')
-        table = await page.inner_text('table')
         html = await page.inner_html('main.page-content')
-        # print(text)
-        print(table)
+        print("Get data autobank successfully")
+        
         # Withdrawcredit
         await page.goto("https://backoffice.nigoal939.com/Withdrawcredit")
         await page.wait_for_url("https://backoffice.nigoal939.com/Withdrawcredit")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(2000)
+        await page.select_option('[aria-controls="depositTable"]', '100')
+        await page.wait_for_timeout(2000)
         html1 = await page.inner_html('main.page-content')
-
+        print("Get data withdraw successfully")
+        
         return [html, html1]
 
 
 def manipulate(html):
     j = 0
     arr_account = []
-    arr_table = []
     soup = BeautifulSoup(html, 'html.parser')
     total_books = soup.find_all('h3', {'class':""})
     arr_book = [i.get_text().strip() for i in total_books]
-    # trans_success = soup.find('h2').text
-    # trans_success = soup.find_all('h2')
-    # spans = soup.find_all('span', {'class' : 'fa fa-credit-card f-left'})
-    for j in range(len(arr_book)):
-        trans_success = soup.find_all('h2')
-        data = trans_success[j].text
-        arr_account.append(data)
-        j += 1
-
-    for i in range(18):
-        # data_table = soup.find_all('tr')
-        data_table = soup.find_all('p',{'class':"text-right"})
-        result_table = data_table[i].text
-        arr_table.append(result_table)
-
-
-    # print(arr_table)
     print(arr_book)
+    
+    for j in soup.find_all('h2'):
+        data = j.text
+        arr_account.append(data)
     print(arr_account)
     
 
 def withdrawcredit(html):
     arr_transec = []
+    table_headers = []
+    table_rows = []
     soup = BeautifulSoup(html, 'html.parser')
+
     transec_deposit = soup.find('span',{'id':"depositTotalByDate"}).text
     transec_withdraw = soup.find('span',{'id':"withdrawTotalByDate"}).text
     arr_transec.append(transec_deposit)
     arr_transec.append(transec_withdraw)
     print(arr_transec)
 
+    table_deposit = soup.find('table',{"id":"depositTable"})
+    for k in table_deposit.find_all('th'):
+        title = k.text
+        table_headers.append(title)
+
+    # Create a dataframe
+    # my_table = pd.DataFrame(columns = table_headers)
+
+    # Create a for loop to fill mydata
+    for m in table_deposit.find_all('tr')[1:]:
+        row_data = m.find_all('td')
+        row = [n.text for n in row_data]
+        # print(row)
+        # length = len(my_table)
+        # my_table.loc[length] = row
+        table_rows.append(row)
 
 
-
-
+    print(table_rows)
+    print(len(table_rows))
+    print(len(table_headers))
+    
+    
+    
+    
+            
 
 data = asyncio.run(gethtml())
 manipulate(data[0])
